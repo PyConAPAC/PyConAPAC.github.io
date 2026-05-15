bdc94cba8d84f627009b8917d938b791017d7041
Micaela Reyes <codemickeycode@gmail.com>
2026-05-15 12:56:23 +0800

    Update Events 2026

--- src/views/events/EventData.js
+++ src/views/events/EventData.js
@@ -1,99 +1,57 @@
-export const currentYear = 2025
+export const currentYear = 2026
 
 // Array of events for the current year's PyCon
 export const events = [
     {
-        date: '1-2 March',
-        title: 'PyCon APAC',
-        location: 'Ateneo de Manila University, Quezon City, Manila, Philippines',
-        link: 'https://pycon-apac.python.ph/',
-        countryCode: 'PH',
+        date: '9 May',
+        title: 'PyCon Busan',
+        location: 'Busan, South Korea',
+        link: 'https://busan.pycon.kr/2026',
+        countryCode: 'KR',
     },
     {
-        date: '18-20 June',
+        date: '19-21 June',
         title: 'PyCon SG',
-        location: 'Singapore Institute of Technology, Singapore',
+        location: 'Singapore',
         link: 'https://pycon.sg',
         countryCode: 'SG',
     },
     {
         date: '15-17 August',
         title: 'PyCon KR',
-        location: 'Dongguk University, Seoul, South Korea',
-        link: 'https://2025.pycon.kr/',
+        location: 'South Korea',
+        link: 'https://2026.pycon.kr',
         countryCode: 'KR',
     },
     {
-        date: '5-7 September',
-        title: 'PyCon TW ',
-        location: 'Taipei, Taiwan',
-        link: 'https://tw.pycon.org/2025/en-us',
-        countryCode: 'TW',
-    },
-    {
-        date: '12-15 September',
-        title: 'PyCon India',
-        location: 'Nimhans Convention Center, Bangalore, India',
-        link: 'https://in.pycon.org/2025/',
-        countryCode: 'IN',
-    },
-    {
-        date: '12-16 September',
-        title: 'PyCon AU',
-        location: 'Melbourne, Australia',
-        link: 'https://2025.pycon.org.au',
-        countryCode: 'AU',
-    },
-    {
-        date: '26-27 September',
+        date: '21-23 August',
         title: 'PyCon JP',
         location: 'Hiroshima, Japan',
-        link: 'https://2025.pycon.jp',
+        link: 'https://2026.pycon.jp',
         countryCode: 'JP',
     },
     {
-      date: '11-12 October',
-      title: 'PyCon HK',
-      location: 'City University of Hong Kong, Hong Kong',  
-      link: 'https://pycon.hk',
-      countryCode: 'HK',
+        date: '26-30 August',
+        title: 'PyCon AU',
+        location: 'Brisbane, Australia',
+        link: 'https://2026.pycon.org.au',
+        countryCode: 'AU',
     },
     {
-      date: '17-18 October',
-      title: 'PyCon TH',
-      location: 'Avani Sukhumvit Bangkok Hotel, Bangkok, Thailand',
-      link: 'https://th.pycon.org',
-      countryCode: 'TH',
+        date: '8-9 August',
+        title: 'PyCon ID',
+        location: 'Jakarta, Indonesia',
+        link: 'https://temp.pycon.id',
+        countryCode: 'ID',
     },
     {
-      date: '25-26 October',
-      title: 'PyCon Davao',
-      location: 'Ateneo de Davao University, Davao City, Philippines',
-      link: 'https://pycon-davao.durianpy.org',
-      countryCode: 'PH',
+        date: 'Oct-Nov',
+        title: 'PyCon HK',
+        location: 'Hong Kong',
+        link: 'https://www.pycon.hk',
+        countryCode: 'HK',
     },
-    {
-      date: '1-2 November',
-      title: 'PyCon MY',
-      location: 'Sunway University, Subang Jaya, Malaysia',
-      link: 'https://pycon.my',
-      countryCode: 'MY',
-    },
-    {
-      date: '21-23 November',
-      title: 'PyCon NZ',
-      location: 'Wellington Waterfront, New Zealand',
-      link: 'https://kiwipycon.nz',
-      countryCode: 'NZ',
-    },
-    {
-      date: '13-14 December',
-      title: 'PyCon ID',
-      location: 'Jakarta, Indonesia',
-      link: 'https://pycon.id',
-      countryCode: 'ID',
-    }
-  ];
+];
 
 
 // Array of upcoming events for the next year
